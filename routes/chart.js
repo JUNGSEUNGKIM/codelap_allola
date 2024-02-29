34946138d2ae2c152db4a94824cdf41dbb1f3b16
AoifeRho <155344084+AoifeRho@users.noreply.github.com>
2024-02-29 17:47:15 +0900

    20240229 17:47 가현

---
+++ routes/chart.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+const router = express.Router();
+
+
+app.set('view engine', 'ejs');
+
+
+router.get('/', async (req, res) => {
+
+    res.render('chart');
+});
+module.exports = router;
